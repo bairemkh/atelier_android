cdbb56d0b7ff8146ece1d5646c52bc050beadfc5
bairemkh <72412872+bairemkh@users.noreply.github.com>
2023-02-21 20:59:41 +0100

    info working

---
+++ app/src/main/java/com/esprit/diceapp/Login.kt
@@ -0,0 +1,92 @@
+package com.esprit.diceapp
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.util.Patterns
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.RadioGroup
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class Login : AppCompatActivity() {
+    lateinit var fullName: TextInputEditText
+    lateinit var email: TextInputEditText
+    lateinit var age: TextInputEditText
+    private val PICK_FROM_GALERY = 100
+    private var imageIsEmpty = true
+    lateinit var imgData: Uri
+    lateinit var image: ImageView
+    var gender: String = "Male"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        fullName = findViewById(R.id.fullNameText)
+        email = findViewById(R.id.emailText)
+        age = findViewById(R.id.ageText)
+        image = findViewById(R.id.imageLogin)
+        image.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, PICK_FROM_GALERY)
+        }
+        findViewById<RadioGroup>(R.id.genderRG).setOnCheckedChangeListener { group, checkedId ->
+            gender = when (checkedId) {
+                R.id.menRB -> "Male"
+                R.id.womenRB -> "Female"
+                else -> "Error"
+            }
+        }
+        findViewById<Button>(R.id.toNext).setOnClickListener {
+            toNext()
+        }
+    }
+    private fun toNext() {
+        var response = true
+        val widgets = listOf(fullName, email, age)
+        val backgrounds = listOf<TextInputLayout>(
+            findViewById(R.id.textInputLayout1),
+            findViewById(R.id.textInputLayout2),
+            findViewById(R.id.textInputLayout3)
+        )
+        for ((widget, background) in widgets.zip(backgrounds)) {
+            if (widget.text!!.isEmpty()) {
+                background.error = "Must not be empty"
+                response = false
+            }
+            else if ((widget == email) && (!widget.text!!.matches(Patterns.EMAIL_ADDRESS.toRegex()))) {
+                background.error = "Must Be an email"
+                response = false
+            }
+            else
+                background.error = null
+        }
+        if(imageIsEmpty)
+            Toast.makeText(this,"Please pick an image",Toast.LENGTH_SHORT).show()
+        if (response&&!imageIsEmpty) {
+            Log.e("Pass", "Passed congrats!!")
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("name", fullName.text.toString())
+            intent.putExtra("email", email.text.toString())
+            intent.putExtra("age", age.text.toString())
+            intent.putExtra("gender", gender)
+            intent.putExtra("imageUri", imgData.toString())
+            startActivity(intent)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode== RESULT_OK){
+            if(requestCode==PICK_FROM_GALERY){
+                imgData=data?.data!!
+                imageIsEmpty = false
+                image.setImageURI(imgData)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/esprit/diceapp/WelcomeFragment.kt
@@ -0,0 +1,24 @@
+package com.esprit.diceapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+class WelcomeFragment(val name:String, val age:String,  val gender:String,val email:String) : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        view.findViewById<TextView>(R.id.welcomingText).text = "Hello, My name is $name"
+        view.findViewById<TextView>(R.id.welcomeInfoText).text = "I have $age years old" +
+                "\nand i'm a $gender" +
+                "\nYou can contact me via my email \n$email"
+        return view
+    }
+}
